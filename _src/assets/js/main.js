eef3d342a3e5c390b861a79573aee589ad8dbfd2
minamrp <ramosp.marina@gmail.com>
2019-02-26 14:16:25 +0100

    vanila javascript version

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -48,39 +48,3 @@ const selectEl = document.querySelector('select');
 selectEl.addEventListener('change', selectValue);
 btn.addEventListener('click', selectOption);
 
-
-
-// define('modulo', ['jquery'], function ($) {
-
-//   var modulo = {
-//       //START
-
-//       //Private attributes
-//       _lista: {
-//           A: "Primer elemento",
-//           B: "Segundo elemento",
-//           C: "Tercer elemento",
-//           D: "Cuarto elemento",
-//           E: "Quinto elemento",
-//       },
-//       _MENSAJE: "Elemento enviado: ",
-
-//       //Public attributes
-
-//       //Public methods
-//       init: function () {
-//           //call to ready method
-
-//           //call to mockup method
-
-//           //call to addEvents method
-
-//       },
-
-//       //Private methods
-
-//       //END
-//   };
-//   modulo.init();
-// });
-
